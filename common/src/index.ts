5f0f38e56fa6d03b650d4e011a86a8354274424e
Simone De Cristofaro <simone.decristofaro85@gmail.com>
2025-08-10 02:56:30 +0200

    Update index.ts

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,2 +1,3 @@
-export * from "./services/logger.service";
-export * as dateTimeUtils from './utils/datetime.utils';
+// common/src/index.ts
+export { Logger, ILogger, type ILogMetadata } from "./services/logger.service";
+export * as dateTimeUtils from "./utils/datetime.utils";
